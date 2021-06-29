920323d7b7c1ec82b3541e9835db1c936d39224c
Omar Magdy <contact.parmag@gmail.com>
2021-06-29 22:47:19 +0200

    cantiin:models.poy:add:min and max values

--- _app/cantiin/models.py
+++ _app/cantiin/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 #id,name,price,in_stock,author
 class Product(models.Model):
-	name = models.CharField(max_length=100)
-	price = models.FloatField()
+	name = models.CharField(max_length=150)
+	price = models.FloatField(min_value=.01,max_value=1000*1000)
 	in_stock =  models.BooleanField()
 
 #id, author, product_id, amount
 class Order(models.Model):
-	amount = models.FloatField()
+	amount = models.IntegerField(min_value=0,max_value=1000)
 
 #id, author, product_id, content
 class Comment(models.Model):
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=1000)
