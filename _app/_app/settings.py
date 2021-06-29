489d0573ece2d973c6e56813209e043756708386
Omar Magdy <contact.parmag@gmail.com>
2021-06-29 20:08:35 +0200

    settings.py:add:drf

--- _app/_app/settings.py
+++ _app/_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cantiin',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- _app/_app/urls.py
+++ _app/_app/urls.py
@@ -16,6 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+
+from rest_framework import routers
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+#router.register(r'users', UserViewSet)
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
