50907f3e36eed0f54ee6e517de1e68947e350a45
Omar Magdy <contact.parmag@gmail.com>
2021-06-30 00:10:08 +0200

    applied models mixins

--- _app/_app/models_mixins.py
+++ _app/_app/models_mixins.py
@@ -3,17 +3,18 @@ from django.db import models
 from django.conf import settings
 
 
-
+from cantiin.models import Product
 
 
 class TimeStampMixin():
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
-class HasProductForeignKeyMixin():
-	product = models.ForeignKey(Product, related_name="orders")
-
 class HasUserForeignKeyMixin():
 	author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+
+class HasProductForeignKeyMixin():
+	product = models.ForeignKey(Product, related_name="orders")
 
 
--- _app/cantiin/models.py
+++ _app/cantiin/models.py
@@ -1,30 +1,29 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
-from django.conf import settings
 
 
 from _app.models_mixins import (TimeStampMixin, 
-	HasProductForeignKeyMixin, HasUserForeignKeyMixin)
+	HasUserForeignKeyMixin, HasProductForeignKeyMixin)
 
 
 
 
 #id,name,price,in_stock,author
-class Product(models.Model):
+class Product(models.Model, HasUserForeignKeyMixin):
 	name = models.CharField(max_length=150)
 	price = models.FloatField(
 		validators=[MinValueValidator(.1),MaxValueValidator(1000*1000)])
 	in_stock =  models.BooleanField()
 
 #id, author, product_id, amount
-class Order(models.Model):
+class Order(models.Model, HasUserForeignKeyMixin, HasProductForeignKeyMixin):
     product = models.ForeignKey(Product, related_name="orders")
 	amount = models.IntegerField(
 		 validators=[MinValueValidator(1),MaxValueValidator(1000)])
 
 #id, author, product_id, content
-class Comment(models.Model):
+class Comment(models.Model, HasUserForeignKeyMixin, HasProductForeignKeyMixin):
     product = models.ForeignKey(Product, related_name="comments")
 	name = models.CharField(max_length=1000)
 
