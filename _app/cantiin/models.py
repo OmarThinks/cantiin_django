b7bc05d9574b6319b90d2e3c947eba69f28a2780
Omar Magdy <contact.parmag@gmail.com>
2021-06-29 22:49:25 +0200

    cantiin:models:mod:removed min and max values

--- _app/cantiin/models.py
+++ _app/cantiin/models.py
@@ -5,12 +5,12 @@ from django.db import models
 #id,name,price,in_stock,author
 class Product(models.Model):
 	name = models.CharField(max_length=150)
-	price = models.FloatField(min_value=.01,max_value=1000*1000)
+	price = models.FloatField()
 	in_stock =  models.BooleanField()
 
 #id, author, product_id, amount
 class Order(models.Model):
-	amount = models.IntegerField(min_value=0,max_value=1000)
+	amount = models.IntegerField()
 
 #id, author, product_id, content
 class Comment(models.Model):
