3ed9afe24ca3bbc14a3ed04058fb0dceff9e9e93
Omar Magdy <contact.parmag@gmail.com>
2021-06-30 14:34:59 +0200

    rendered a template

--- _app/cantiin/views.py
+++ _app/cantiin/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def about(request):
 	return HttpResponse("About")
 
 def homepage(request):
-	return HttpResponse("Home Page")
+	#return HttpResponse("Home Page")
+	return render(request, "home.html")
+
