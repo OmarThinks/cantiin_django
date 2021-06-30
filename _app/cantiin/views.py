f047bec1a80ea43e8ef269d856e53e7d6e5548f1
Omar Magdy <contact.parmag@gmail.com>
2021-06-30 02:13:34 +0200

    vies.py:add:viewsets

--- _app/cantiin/views.py
+++ _app/cantiin/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets, routers
-"""
+from rest_framework import viewsets
 # Serializers define the API representation.
-from 
+
+
+from accounts.models import (User)
+from cantiin.models import (Product, Order, Comment)
+
+from cantiin.serializers import (
+	UserSerializer, ProductSerializer, OrderSerializer, CommentSerializer)
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-"""
-
 
 
 
