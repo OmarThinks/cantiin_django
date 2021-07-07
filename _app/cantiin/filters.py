a1da91e05ed2976bb2d31fa836c577dd9d59e7b9
Omar Magdy <contact.parmag@gmail.com>
2021-07-07 20:11:54 +0200

    filters.py:add:created at

--- _app/cantiin/filters.py
+++ _app/cantiin/filters.py
@@ -3,12 +3,17 @@ from accounts.models import (User)
 from cantiin.models import (Product, Order, Comment)
 
 
+
+
 class ProductFilter(filters.FilterSet):
 	min_price = filters.NumberFilter(field_name="price", 
 		lookup_expr='gte')
 	max_price = filters.NumberFilter(field_name="price", 
 		lookup_expr='lte')
-
+	min_created_at = filters.DateTimeFilter(field_name="created_at", 
+		lookup_expr='gte')
+	max_created_at = filters.DateTimeFilter(field_name="created_at", 
+		lookup_expr='lte')
 	class Meta:
 		model = Product
 		fields = ['in_stock', 'min_price', 'max_price',"author_id"]
