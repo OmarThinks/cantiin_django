4ace5cb7e0c957b865f544aacac822ae5a860a09
Omar Magdy <contact.parmag@gmail.com>
2021-08-03 08:22:14 +0200

    removed django_restql leftovers

--- _app/cantiin/serializers.py
+++ _app/cantiin/serializers.py
@@ -3,7 +3,6 @@ from cantiin.models import (Product, Order, Comment)
 
 from rest_framework import serializers
 from my_functions.urls import reverse
-from django_restql.mixins import DynamicFieldsMixin
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -90,75 +89,4 @@ class CommentSerializer(serializers.HyperlinkedModelSerializer):
 					"product": {"view_name":"api:product-detail"}
 
 		}
-
-
-
-"""
-python manage.py shell
-from cantiin.serializers import (UserSerializer, ProductSerializer, OrderSerializer, CommentSerializer)
-serializers= [UserSerializer(), ProductSerializer(), OrderSerializer(), CommentSerializer()]
-print(repr(serializers[0]),"\n")
-print(repr(serializers[1]),"\n")
-print(repr(serializers[2]),"\n")
-print(repr(serializers[3]),"\n")
-exit()
-
-
-
-
-
-
->>> >>> >>> UserSerializer():
-    url = HyperlinkedIdentityField(view_name='user-detail')
-    id = IntegerField(label='ID', read_only=True)
-    username = CharField(help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, validators=[<django.contrib.auth.validators.UnicodeUsernameValidator object>, <UniqueValidator(queryset=User.objects.all())>])
-    products = SerializerMethodField(read_only=True)
-    products_count = SerializerMethodField(read_only=True)
-    orders = SerializerMethodField(read_only=True)
-    orders_count = SerializerMethodField(read_only=True)
-    comments = SerializerMethodField(read_only=True)
-    comments_count = SerializerMethodField(read_only=True)
-
->>> ProductSerializer():
-    id = IntegerField(label='ID', read_only=True)
-    url = HyperlinkedIdentityField(view_name='product-detail')
-    name = CharField(max_length=150)
-    price = FloatField(max_value=1000000, min_value=0.1)
-    in_stock = BooleanField()
-    author_id = ReadOnlyField()
-    author = HyperlinkedRelatedField(read_only=True, view_name='user-detail')
-    created_at = DateTimeField(read_only=True)
-    updated_at = DateTimeField(read_only=True)
-    comments = SerializerMethodField(read_only=True)
-    comments_count = SerializerMethodField(read_only=True)
-    orders = SerializerMethodField(read_only=True)
-    orders_count = SerializerMethodField(read_only=True)
-
->>> OrderSerializer():
-    url = HyperlinkedIdentityField(view_name='order-detail')
-    id = IntegerField(label='ID', read_only=True)
-    product_id = ReadOnlyField()
-    product = HyperlinkedRelatedField(queryset=Product.objects.all(), view_name='product-detail')
-    unit_price = ReadOnlyField()
-    amount = IntegerField(max_value=1000, min_value=1)
-    cost = ReadOnlyField()
-    author_id = ReadOnlyField()
-    author = HyperlinkedRelatedField(read_only=True, view_name='user-detail')
-    created_at = DateTimeField(read_only=True)
-    updated_at = DateTimeField(read_only=True)
-
->>> CommentSerializer():
-    url = HyperlinkedIdentityField(view_name='comment-detail')
-    id = IntegerField(label='ID', read_only=True)
-    product_id = ReadOnlyField()
-    product = HyperlinkedRelatedField(queryset=Product.objects.all(), view_name='product-detail')
-    author_id = ReadOnlyField()
-    author = HyperlinkedRelatedField(read_only=True, view_name='user-detail')
-    content = CharField(max_length=1000)
-    created_at = DateTimeField(read_only=True)
-    updated_at = DateTimeField(read_only=True)
-
-
-
-"""
 
