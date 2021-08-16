b56c7a057cc36b141d5364ca819729f93769d256
Omar Magdy <contact.parmag@gmail.com>
2021-08-16 10:13:35 +0200

    login.html:updated

--- _app/frontend/views_auth.py
+++ _app/frontend/views_auth.py
@@ -2,6 +2,8 @@ from djoser.views import UserViewSet as DjoserViewSet
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
 from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework import serializers
 
 
 class AuthViewSet(DjoserViewSet):
@@ -11,14 +13,36 @@ class AuthViewSet(DjoserViewSet):
 
 
 
-
-
-
+class LoginSerializer(serializers.Serializer):
+    Username_or_Email = serializers.CharField(
+        max_length=100,
+        style={'placeholder': 'Username or Email', 'autofocus': True}
+    )
+    password = serializers.CharField(
+        max_length=100,
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+    remember_me = serializers.BooleanField()
 
 
 def login(request):
-	return HttpResponse("Login")
+	#return HttpResponse("Login")
+	return render(request, "pages/login.html",
+		{
+			"active_main_navbar": "login",
+			"title": "Login",
+			"serializer":LoginSerializer
+		}			
+	)
 
+def login_old(request):
+	#return HttpResponse("Login")
+	return render(request, "pages/login.html",
+		{
+			"active_main_navbar": "login",
+			"title": "Login"
+		}			
+	)
 
 
 
