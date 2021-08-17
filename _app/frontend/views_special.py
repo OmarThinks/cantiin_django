8afefd6b1f832f31588c3ac51b8c942b21287d1d
Omar Magdy <contact.parmag@gmail.com>
2021-08-17 11:59:21 +0200

    my_products is now completely_ready

--- _app/frontend/views_special.py
+++ _app/frontend/views_special.py
@@ -1,12 +1,20 @@
 from cantiin.models import (Product)
-from .views import ProductViewSet as _ProductViewSet
+from .views import ProductViewSet as _ProductViewSet, generate_custom_renderer
 from django.shortcuts import redirect
 from django.http import (HttpResponse)
 
 
 class MyProductsViewSet(_ProductViewSet):
 	def get_queryset(self):
+		print(self.__dir__(), flush=True)
 		user_id = self.request.user.id
 		return Product.objects.filter(author_id=user_id).all()
+	def get_renderers(self):
+		customRenderer = generate_custom_renderer(
+			items_plural="products",active_main_navbar= "my_products",
+			title="My Products", additional_css_files=[], 
+			item_url_name="frontend:product-detail")
+		renderers = [customRenderer]
+		return [renderer() for renderer in renderers]
 
 my_products = MyProductsViewSet.as_view({"get":"list"})
