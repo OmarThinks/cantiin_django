b4e4692c2bf2745d6b94b513853b3bf589c66828
Omar Magdy <contact.parmag@gmail.com>
2021-08-17 18:05:28 +0200

    forms.py:add:renderers

--- _app/my_functions/forms.py
+++ _app/my_functions/forms.py
@@ -1,23 +1,39 @@
 from pprint import pprint as pp
 
 
-def render_text(field_name, field_name_capitalized):
-	return ""
-def render_number(field_name, field_name_capitalized):
-	return ""
-def render_checkbox(field_name, field_name_capitalized):
-	return ""
+def render_text(field_name, field_name_capitalized, field_type):
+	return f'''
+
+		<label>{field_name_capitalized}</label>
+		<input name="{field_name}" class="form-control" 
+		type="{field_type}"  value="" >
+
+	'''
+
+def render_number(field_name, field_name_capitalized,field_type):
+	return render_text(field_name, field_name_capitalized, field_type)
+
+def render_checkbox(field_name, field_name_capitalized, field_type):
+	return f'''
+		
+		<div class="{field_type}">
+		<label>
+		  <input type="{field_type}" name="{field_name}" value="true" >
+		    {field_name_capitalized}
+		</label>
+		</div>
+
+	'''
 
 
 def render_field(field_name:str, field_name_capitalized:str, field_type:str):
 	if field_type == "text":
-		return render_text(field_name, field_name_capitalized)
+		return render_text(field_name, field_name_capitalized, field_type)
 	if field_type == "number":
-		return render_number(field_name, field_name_capitalized)
+		return render_number(field_name, field_name_capitalized, field_type)
 	if field_type == "checkbox":
-		return render_checkbox(field_name, field_name_capitalized)
+		return render_checkbox(field_name, field_name_capitalized, field_type)
 	raise Exception("This field is unknown: "+str(field_name))
-
 
 
 def form_renderer(form_list):
@@ -26,8 +42,22 @@ def form_renderer(form_list):
 		field_name = element["name"]
 		field_name_capitalized = element["name_capitalized"]
 		field_type = element["type"]
+		final_form += '''
+		
+		<div class="form-group ">
+
+		'''
 		final_form += render_field(field_name, 
 			field_name_capitalized, field_type)
+
+		final_form += '''
+		
+		</div>
+
+		'''
+
+
+
 	return final_form
 
 
