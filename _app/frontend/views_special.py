a74e204adc5f86ec69809e9b2ce4b94201767c88
Omar Magdy <contact.parmag@gmail.com>
2021-08-17 11:11:43 +0200

    my_products.html:add:my products endpoint

--- _app/frontend/views_special.py
+++ _app/frontend/views_special.py
@@ -6,15 +6,7 @@ from django.http import (HttpResponse)
 
 class MyProductsViewSet(_ProductViewSet):
 	def get_queryset(self):
-		print(self.request.user.__dir__(),flush=True)
-		return _ProductViewSet.get_queryset(self)
+		user_id = self.request.user.id
+		return Product.objects.filter(author_id=user_id).all()
 
-
-
-def my_products(request):
-	if not request.user.is_authenticated:
-		return redirect("/products/")
-	user_id = request.user.id
-	products = Product.objects.filter(author_id=user_id).all()
-	print(products, flush=True)
-	return HttpResponse("My Products")
+my_products = MyProductsViewSet.as_view({"get":"list"})
