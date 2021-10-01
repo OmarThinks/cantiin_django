4958380a9dcff005d97623a96a29bda1e17b9358
Omar Magdy <contact.parmag@gmail.com>
2021-10-01 16:33:24 +0200

    refractored login serializer

--- _app/accounts/urls.py
+++ _app/accounts/urls.py
@@ -11,26 +11,7 @@ class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, validators=[UnicodeUsernameValidator])
     password = serializers.CharField(max_length=128)
 
-    def validate_username(self, value):
-            username = self.initial_data["username"]
-            if(type(username)!=str):
-                raise serializers.ValidationError("username should be a string")
-            return value
-    def validate_password(self, value):
-            username = self.initial_data["username"]
-            password = self.initial_data["password"]
-            if(type(password)!=str):
-                raise serializers.ValidationError("password should be a string")
-            user = User.objects.filter(username=username).first()
-            if user is None:
-                raise serializers.ValidationError("wrong username or password")
-            if not user.check_password(password):
-                raise serializers.ValidationError("wrong username or password")
-            return user
 
-#from accounts.urls import LoginSerializer
-#serializer = LoginSerializer()
-#print(repr(serializer))
 @api_view(http_method_names=['POST'])
 @authentication_classes([])
 @permission_classes([])
@@ -39,24 +20,17 @@ def LoginView(request):
     serializer.is_valid(raise_exception=True)
     data = serializer.validated_data
     username = data["username"]
-    user = data["password"]
-    # This is not the password, this is the user instance
-    #print("password", user)
-    #print(type(user))
-    #print(serializer.__dict__)
-    """wrong_password_response = Response()
-    wrong_password_response.status_code =400
-    wrong_password_response.data = {"password":["wrong username or passwordaaaa"]}
-    user = User.objects.filter(username=username).first()
-    #print(user)
-    if user is None:
-        return wrong_password_response
-    if not user.check_password(password):
-        return wrong_password_response"""
-    login(request, user)
+    password = data["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        return Response({"message": "You are logged in"})
     
-    #print(data, flush=True)
-    return Response({"message": "You are logged in"})
+    wrong_password_response = Response( )
+    wrong_password_response.status_code =400
+    wrong_password_response.data = {"password":["wrong username or password"]}
+    return wrong_password_response
 
 
 
