c37536a9e5bb19fda0b6759481b18b1bb4092733
Omar Magdy <contact.parmag@gmail.com>
2021-10-01 14:01:05 +0200

    fixed middleware mistake

--- _app/_app/middlewares.py
+++ _app/_app/middlewares.py
@@ -6,7 +6,11 @@ class CORSMiddleware:
     def __call__(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True) # Disable CSRF
         response = self.get_response(request)
-        response['access-control-allow-origin'] = "http://127.0.0.1:3000"
+        referer = request.headers.get("Referer","*")
+        if(referer[-1]=="/"):
+            referer = referer[:-1]
+        response = self.get_response(request)
+        response['access-control-allow-origin'] = referer
         response['access-control-allow-headers'] = "*,content-type,sessionid"
         response['Access-Control-Allow-Credentials'] = "true"
         """["accept",
@@ -20,3 +24,10 @@ class CORSMiddleware:
     "x-requested-with",
 ]"""
         return response
+    
+"""
+        referer = request.headers.get("Referer","*")
+        response = self.get_response(request)
+# http://127.0.0.1:3000
+# http://127.0.0.1:3000/
+"""
--- _app/_app/settings.py
+++ _app/_app/settings.py
@@ -73,11 +73,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+"""
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.RemoteUserBackend',
 ]
-
+"""
 
 
 ROOT_URLCONF = '_app.urls'
@@ -173,10 +173,10 @@ from _app.permissions import CanttinPermission
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.RemoteUserAuthentication',        
+        #'rest_framework.authentication.RemoteUserAuthentication',        
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',        
-        'rest_framework.authentication.TokenAuthentication',        
+        #'rest_framework.authentication.TokenAuthentication',        
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
