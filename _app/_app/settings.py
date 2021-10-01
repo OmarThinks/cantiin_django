1b33d6de12da0bfc043f8a00243b1e1c6b4c459f
Omar Magdy <contact.parmag@gmail.com>
2021-10-01 15:12:32 +0200

    access control allow credentials: True

--- _app/_app/settings.py
+++ _app/_app/settings.py
@@ -213,7 +213,6 @@ SESSION_COOKIE_SAMESITE = 'None'
 
 
 
-
 CORS_ALLOWED_ORIGINS = [
     "https://cantiin.com",
     "https://www.example.com",
@@ -224,3 +223,21 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
 ]
+
+"""
+CORS_ALLOW_HEADERS = ["*",
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+"""
+
+
+CORS_ALLOW_CREDENTIALS= True
